97f310fab780ea254ebcb021a006712451792606
Alex Somheil <alexlsomheil@gmail.com>
2025-02-20 11:06:12 -0600

    fixed device setup guides being sent out

--- shaman2/common/config.py
+++ shaman2/common/config.py
@@ -5,9 +5,6 @@ from shaman2.common.paths import paths
 with open(paths["config"] / "main.toml","r") as f:
     mainConfig = tomlkit.parse(f.read())
 
-with open(paths["config"] / "emailTemplates.toml","r") as f:
-    emailTemplatesConfig = tomlkit.parse(f.read())
-
 
 # Handle OTP generators.
 if("jumpcloudOTPCode" in mainConfig["authentication"] and mainConfig["authentication"]["jumpcloudOTPCode"].strip()):
